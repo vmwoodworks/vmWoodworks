b9a2461c697d85f655fad1f984d0081cb67c0813
aldu1n <cnndo14@gmail.com>
2025-04-09 00:26:48 -0400

    portfolio page made respansive for mobile

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -2,43 +2,39 @@ import { Link } from "react-router-dom";
 import '../css/portfolio.css';
 import React from "react";
 import { useQuery } from "@apollo/client";
-import { GET_ITEMS } from "../utils/queries"; // Adjust the path to where your query is defined
+import { GET_ITEMS } from "../utils/queries";
 
 const Portfolio = () => {
-
   const { loading, error, data } = useQuery(GET_ITEMS);
-
+  
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
-
+  
   function changeLastZeroToOne(url) {
     return url.replace(/(0)(?=[^0]*$)/, "1");
   }
-
+  
   return (
-    <div id="portolio-page" style={{marginBottom: '100px'}}>
-      <h2 className="heading" style={{marginLeft: '150px'}}>Portfolio</h2>
+    <div id="portfolio-page" className="portfolio-container">
+      <h2 className="heading">Portfolio</h2>
       <div className="portfolio-div">
         {data.items.map((item) => (
-          <div className="portfolio-item"
-            key={item._id}
-          >
+          <div className="portfolio-item" key={item._id}>
             <Link to={`/details/${item._id}`}>
               <img
                 src={changeLastZeroToOne(item.mainImage)}
                 alt="Main"
               />
               <div className="overlay">
-              <p className="details-p">{item.description}</p>
-              <p className="category-p">{item.category}</p>
+                <p className="details-p">{item.description}</p>
+                <p className="category-p">{item.category}</p>
               </div>
             </Link>
           </div>
-    ))}
+        ))}
       </div>
     </div>
-    );
-
+  );
 };
 
 export default Portfolio;
